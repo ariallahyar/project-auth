3224699289503fc0ead5706053cd40ab8454e474
Arianna Allahyar <anallahyar@gmail.com>
2022-05-27 08:28:21 +0200

    Redirect if token is not present

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -5,26 +5,31 @@ import { BASE_URL } from "utils/urls";
 
 const Welcome = () => {
   const [message, setMessage] = useState("");
-
+  const token = localStorage.getItem("token");
   const navigate = useNavigate();
+
+  if (!token) {
+    navigate("/");
+  }
 
   const options = {
     method: "GET",
-    headers: { "Token": localStorage.getItem("token") },
+    headers: { Token: token },
   };
 
   useEffect(() => {
     fetch(`${BASE_URL}/session`, options)
-    .then((response) => response.json())
-    .then((data) => setMessage(data.message))
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message));
   }, []);
 
   return (
     <>
       <h2>{message}</h2>
-      <button onClick={() => {
-        localStorage.removeItem("token");
-        navigate("/signin")
+      <button
+        onClick={() => {
+          localStorage.removeItem("token");
+          navigate("/signin");
         }}
       >
         Sign out
